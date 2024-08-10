cc19936f812410ea9131ecf86419d3614de93030
Joseph Orokpo <josephorokpo@gmail.com>
2024-08-10 21:59:01 +0100

    Integrate Editor component: Added Editor component to the App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import Editor from './components/Editor';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Markdown Previewer</h1>
+        <Editor />
+      {/* Preview component will be added here */}
       </header>
       {/* Additional components will be added here */}
     </div>
