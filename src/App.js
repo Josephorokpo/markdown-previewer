8e129ef3573ee7bda08089e9d2e2f53040a51319
Joseph Orokpo <josephorokpo@gmail.com>
2024-08-10 21:55:43 +0100

    Add header section: Included a header section in the App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <h1>Markdown Previewer</h1>
+      </header>
       {/* Additional components will be added here */}
     </div>
   );
 }
-
-export default App;
