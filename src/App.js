646dfbc0817e4718d8f649ad6949c62081a5f473
Joseph Orokpo <josephorokpo@gmail.com>
2024-08-10 22:00:36 +0100

    Integrate Preview component: Added Preview component to the App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Editor from './components/Editor';
+import Preview from './components/Preview';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Markdown Previewer</h1>
         <Editor />
-      {/* Preview component will be added here */}
+        <Preview />
       </header>
-      {/* Additional components will be added here */}
     </div>
   );
 }
